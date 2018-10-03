837d8a68521fb549397377fdb053c761c20e8e4f
senoagung27 <senoagung27@gmail.com>
2018-10-03 19:09:43 +0700

    version 0.1

--- Example/index.android.js
+++ Example/index.android.js
@@ -16,7 +16,7 @@ class Example extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome Try Navigator
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
